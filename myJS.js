388fafc0d4c0b2aa2fddb9a1a70146eff7d2bd4d
SamarAssi <samar.assi2001@gmail.com>
2021-05-01 13:51:28 +0300

    testing

--- myJS.js
+++ myJS.js
@@ -1,3 +1,14 @@
-function show(){
-    for()
+const about=document.getElementsByClassName('about');
+function print(index){
+    for(let i=0;i<about.length;i++){
+        if(i==index){
+            console.log('about #${i} has showed')
+            about[i].style.display='flex';
+            document.getElementsByTagName('button')[i+1].classList='border';
+        }else{
+            console.log('about #${i} has vanished')
+            about[i].style.display='none';
+            document.getElementsByTagName('button')[i+1].classList=' ';
+        }
+    }
 }
